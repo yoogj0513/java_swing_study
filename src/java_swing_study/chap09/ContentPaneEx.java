f3591e55c06e5e84c1680315356815b2517f66f8
yoogj0513 <yoogj0513@naver.com>
2020-01-07 16:26:56 +0900

    이벤트헨들러 추가

--- src/java_swing_study/chap09/ContentPaneEx.java
+++ src/java_swing_study/chap09/ContentPaneEx.java
@@ -4,23 +4,74 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ContentPaneEx extends JFrame {
+public class ContentPaneEx extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JButton btnOK;
+	private JButton btnCancel;
+	private JButton btnIgnore;
 
 	/**
 	 * Create the frame.
 	 */
 	public ContentPaneEx() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창의 엑스버튼을 누르면 클로즈
-		setBounds(100, 100, 450, 300); // 가로좌표, 세로좌표, 가로너비, 세로너비
+		initialize();
+	}
+	private void initialize() {
+		setTitle("ContentPane과 JFrame");
+		// 창의 엑스버튼을 누르면 클로즈
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		// 가로좌표, 세로좌표, 가로너비, 세로너비
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5)); // EmptyBorder(상, 하, 좌, 우); == css패팅과 같음
-		contentPane.setLayout(new BorderLayout(0, 0)); // BorderLayout은 총 다섯 영역이 있음. 위, 아래, 좌, 우, 중앙
-		setContentPane(contentPane);// contentPane을 넣어라. set
+		contentPane.setBackground(Color.ORANGE);
+		// 패딩과 같은 기능
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		// contentPane을 넣어라. set
+		setContentPane(contentPane);
+		// 중앙 위치에 간격 5
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5)); 
+		
+		btnOK = new JButton("OK");
+		btnOK.addActionListener(this);
+		contentPane.add(btnOK);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(this);
+		contentPane.add(btnCancel);
+		
+		btnIgnore = new JButton("Ignore");
+		btnIgnore.addActionListener(this);
+		contentPane.add(btnIgnore);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnIgnore) {
+			btnIgnoreActionPerformed(e);
+		}
+		if (e.getSource() == btnCancel) {
+			btnCancelActionPerformed(e);
+		}
+		if (e.getSource() == btnOK) {
+			btnOKActionPerformed(e);
+		}
+	}
+	protected void btnOKActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnCancelActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnIgnoreActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
 }
