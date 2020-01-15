7939c92a843a839b72df6923688e116f8511dba0
yoogj0513 <yoogj0513@naver.com>
2020-01-15 17:52:57 +0900

    2020/01/15 리스트 수업 파일 마무리

--- src/java_swing_study/chap11/ListEx.java
+++ src/java_swing_study/chap11/ListEx.java
@@ -31,12 +31,12 @@ public class ListEx extends JFrame implements ActionListener, ListSelectionListe
 	private JPanel contentPane;
 	private ArrayList<String> listFruits;
 	private ArrayList<ImageIcon> listImg;
-	
+
 	private JPanel p1;
 	private JPanel pBtns1;
 	private JButton btnOk1;
 	private JButton btnSet1;
-	//String 제네릭 추가
+	// String 제네릭 추가
 	private JList<String> strList;
 	private JPanel p2;
 	private JPanel pBtns2;
@@ -44,13 +44,12 @@ public class ListEx extends JFrame implements ActionListener, ListSelectionListe
 	private JList<ImageIcon> imgList;
 	private JButton btnOk2;
 	private JButton btnSet2;
-	
+
 	private JPanel p3;
 	private JTextField tf;
-	private JList<String> list ;
-	
+	private JList<String> list;
+
 	private ArrayList<String> v;
-	
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -81,95 +80,98 @@ public class ListEx extends JFrame implements ActionListener, ListSelectionListe
 		listFruits.add("berry");
 		listFruits.add("strawberry");
 		listFruits.add("blackberry");
-		
+
 		String imgDirPath = System.getProperty("user.dir") + "\\images\\";
 		listImg = new ArrayList<ImageIcon>();
 		listImg.add(new ImageIcon(imgDirPath + "icon1.png"));
 		listImg.add(new ImageIcon(imgDirPath + "icon2.png"));
 		listImg.add(new ImageIcon(imgDirPath + "icon3.png"));
 		listImg.add(new ImageIcon(imgDirPath + "icon4.png"));
-		
+
 		v = new ArrayList<String>();
 	}
+
 	private void initialize() {
 		setTitle("JList & JCombobox");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		contentPane.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "", TitledBorder.LEADING,
+				TitledBorder.TOP, null, new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(0, 4, 10, 10));
-		
+
 		p1 = new JPanel();
 		p1.setBorder(new TitledBorder(null, "JList\uC608\uC81C", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.add(p1);
 		p1.setLayout(new BorderLayout(0, 0));
-		
+
 		// 벡터에 ArrayList 추가
 		strList = new JList<>(new Vector<>(listFruits));
 		strList.addListSelectionListener(this);
 		p1.add(strList, BorderLayout.CENTER);
-		
+
 		pBtns1 = new JPanel();
 		p1.add(pBtns1, BorderLayout.NORTH);
 		pBtns1.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		btnOk1 = new JButton("확인");
 		btnOk1.addActionListener(this);
 		pBtns1.add(btnOk1);
-		
+
 		btnSet1 = new JButton("선택하기");
 		btnSet1.addActionListener(this);
 		pBtns1.add(btnSet1);
-		
-		
-		
+
 		p2 = new JPanel();
 		p2.setBorder(new TitledBorder(null, "JLIst\uC608\uC81C2", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.add(p2);
 		p2.setLayout(new BorderLayout(0, 0));
-		
+
 		pBtns2 = new JPanel();
 		p2.add(pBtns2, BorderLayout.NORTH);
 		pBtns2.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		btnOk2 = new JButton("확인");
 		btnOk2.addActionListener(this);
 		pBtns2.add(btnOk2);
-		
+
 		btnSet2 = new JButton("선택하기");
+		btnSet2.addActionListener(this);
 		pBtns2.add(btnSet2);
-		
+
 		scrollPane = new JScrollPane();
 		p2.add(scrollPane, BorderLayout.CENTER);
-		
-		imgList = new JList<ImageIcon>();
+
+		imgList = new JList<>(new Vector<>(listImg));
 		scrollPane.add(imgList);
-		
+
 		scrollPane.setViewportView(imgList);
-		
+
 		p3 = new JPanel();
 		contentPane.add(p3);
 		p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
-		
+
 		tf = new JTextField();
 		tf.addActionListener(this);
 		p3.add(tf);
 		tf.setColumns(10);
-		
-		
+
 		JPanel panel_1 = new JPanel();
 		p3.add(panel_1);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		panel_1.add(scrollPane_1);
-		
+
 		list = new JList<>(new Vector<>(v));
 		scrollPane_1.setViewportView(list);
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnSet2) {
+			btnSet2ActionPerformed(e);
+		}
 		if (e.getSource() == tf) {
 			tfActionPerformed(e);
 		}
@@ -183,33 +185,45 @@ public class ListEx extends JFrame implements ActionListener, ListSelectionListe
 			btnOk1ActionPerformed(e);
 		}
 	}
+
 	protected void btnOk1ActionPerformed(ActionEvent e) {
 		int index = strList.getSelectedIndex();
 		String message = listFruits.get(index);
 		JOptionPane.showMessageDialog(null, "actionPerformed - 선택한 과일은 " + message);
 		System.out.println(index + message);
 	}
+
 	public void valueChanged(ListSelectionEvent e) {
 		if (e.getSource() == strList) {
 			strListValueChanged(e);
 		}
 	}
+
 	protected void strListValueChanged(ListSelectionEvent e) {
 		String selectValue = strList.getSelectedValue();
 		JOptionPane.showMessageDialog(null, "valueChanged - 선택한 과일은 " + selectValue);
 	}
+
 	protected void btnSet1ActionPerformed(ActionEvent e) {
-		strList.setSelectedIndex(listFruits.indexOf("peach"));	
+		strList.setSelectedIndex(listFruits.indexOf("peach"));
 	}
+
 	protected void btnOk2ActionPerformed(ActionEvent e) {
-//		int index = imgList.getSelectedIndex();
-//		System.out.println(image[index]);
+		int index = imgList.getSelectedIndex();
+		ImageIcon icon = listImg.get(index);
+		String fullName = icon.getDescription();
+		JOptionPane.showMessageDialog(null, fullName.substring(fullName.lastIndexOf("\\") + 1));
 	}
+
 	protected void tfActionPerformed(ActionEvent e) {
 		String newStr = tf.getText().trim();
 		v.add(newStr);
-		
+
 		list.setListData(new Vector<>(v));
 		tf.setText("");
+	}
+
+	protected void btnSet2ActionPerformed(ActionEvent e) {
+		imgList.setSelectedIndex(2);
 	}
 }
